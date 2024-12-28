41ea6f5e6b5d2d5b0d715ff3b743bd9312a95b2d
Prashant Anand <prashantanandcse2025@gmail.com>
2024-12-29 02:58:06 +0530

    Added backend and frontend folders with code

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        //'mongodb://127.0.0.1:27017/internship'
+        // 'mongodb://127.0.0.1:27017/internship'
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
-        process.exit(1);
     }
 };
 
 module.exports = connectDB;
-
-  //'mongodb://127.0.0.1:27017/internship'
